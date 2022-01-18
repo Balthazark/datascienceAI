d45df8725634218262be6638e5d628601dae4468
hshanz <david00gbg@hotmail.com>
2022-01-18 16:30:42 +0100

    Data cleaned

--- Assignment1/worldOfData.py
+++ Assignment1/worldOfData.py
@@ -18,10 +18,13 @@ lifeExp = readfile('life-expectancy.csv')
 gdp = readfile('gdp-per-capita-worldbank.csv')
 result = []
 
+
+rowtmp = [""]
 for rows in reversed(lifeExp):
     for row in reversed(gdp):
-        if rows[0] == row[0] and rows[2] == row[2]:
-            tmp = [rows[0],rows[3],row[3]] 
+        if rows[0] == row[0] and rows[2] == row[2] and rowtmp[0] != rows[0]:
+            tmp  = [rows[0],rows[3],row[3]] 
             result.append(tmp)
+            rowtmp = rows
+            break
 print(result)
-# %%
