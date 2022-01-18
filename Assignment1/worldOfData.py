3b3d31ed633b926ab264613259c4ebfe42ef32ae
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2022-01-18 15:37:17 +0100

    began

--- Assignment1/worldOfData.py
+++ Assignment1/worldOfData.py
@@ -1,5 +1,21 @@
-def readfile():
-    print("DAVID ÄR SÖT")
+# %%
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
 
+def readfile():
+    #
+    with open('life-expectancy.csv') as file:
+        tmp = [""]
+        result = []
+        reader = csv.reader(file)
+        for row in reader: 
+            if row[0] != tmp[0]:
+                result.append(row)
+            else:
+                tmp = row
+        print(result) 
 readfile()
 
+# %%
