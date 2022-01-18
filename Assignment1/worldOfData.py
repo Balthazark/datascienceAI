04342d09d5139be24134b446a7f393470fe4e936
hshanz <david00gbg@hotmail.com>
2022-01-18 16:02:50 +0100

    working

--- Assignment1/worldOfData.py
+++ Assignment1/worldOfData.py
@@ -4,18 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-def readfile():
+def readfile(f):
     #
-    with open('life-expectancy.csv') as file:
-        tmp = [""]
+    with open(f) as file:
         result = []
         reader = csv.reader(file)
-        for row in reader: 
-            if row[0] != tmp[0]:
-                result.append(row)
-            else:
-                tmp = row
-        print(result) 
-readfile()
+        for row in reader:
+            result.append(row)
+    return result
 
+    
+lifeExp = readfile('life-expectancy.csv')
+gdp = readfile('gdp-per-capita-worldbank.csv')
+result = []
+
+for rows in reversed(lifeExp):
+    for row in reversed(gdp):
+        if rows[0] == row[0] and rows[2] == row[2]:
+            tmp = [rows[0],rows[3],row[3]] 
+            result.append(tmp)
+print(result)
 # %%
