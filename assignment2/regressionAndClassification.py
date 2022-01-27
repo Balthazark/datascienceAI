c17ff188a55887623dc3c0e2c3157d806a2883db
hshanz <david00gbg@hotmail.com>
2022-01-27 15:24:36 +0000

    start webscraping

--- assignment2/regressionAndClassification.py
+++ assignment2/regressionAndClassification.py
@@ -1,3 +1,11 @@
+from email import header
 import pandas as pd
+import requests
+from io import StringIO
 
-webScraper = pd.read_html("")
+url = 'https://www.hemnet.se/salda/bostader?location_ids%5B%5D=940808&item_types%5B%5D=villa&sold_age=6m'
+#headers={'User-Agent': 'Mozilla/5.0'}
+req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+data = StringIO(req.text)
+dfs = pd.read_html(data)
