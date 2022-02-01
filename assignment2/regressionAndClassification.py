353916023a3c8340d04459233557dec9f0e65ef9
hshanz <david00gbg@hotmail.com>
2022-02-01 13:50:37 +0000

    reg

--- assignment2/regressionAndClassification.py
+++ assignment2/regressionAndClassification.py
@@ -1,10 +1,27 @@
 import pandas as pd
-import requests
-from io import StringIO
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import numpy as np
 
-url = 'https://www.hemnet.se/salda/bostader?location_ids%5B%5D=940808&item_types%5B%5D=villa&sold_age=6m'
-#headers={'User-Agent': 'Mozilla/5.0'}
-req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+#From sckit wiki
+def regFit():
+    data = pd.read_csv('DatasetRegression.csv')
+    livingArea = list(map(float,data['Living Area']))
+    livingArea = np.reshape(livingArea,(-1,1))
+    price = list(map(float,data['Price']))
+    price = np.reshape(livingArea,(-1,1))
 
-data = StringIO(req.text)
-dfs = pd.read_html(data)
+    print(livingArea)
+    print(price)
+
+    reg = linear_model.LinearRegression()
+    reg.fit(livingArea,price)
+    pricePredict = reg.predict(livingArea)
+
+    plt.scatter(livingArea,price)
+    plt.plot(livingArea,pricePredict,color='red')
+    plt.show()
+
+
+regFit()
+
